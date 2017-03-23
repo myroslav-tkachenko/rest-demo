b46f0b463b848655d5ade9a4624724267bd710d9
Myroslav Tkachenko <myroslav.tkachenko@gmail.com>
2017-03-24 01:59:36 +0200

    Add PhpRenderer

--- public/index.php
+++ public/index.php
@@ -2,10 +2,19 @@
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use Slim\Views\PhpRenderer;
 
 require '../vendor/autoload.php';
 
 $app = new \Slim\App;
+$container = $app->getContainer();
+$container['renderer'] = new PhpRenderer("../templates");
+
+$app->get('/', function(Request $request, Response $response) {
+    $data = ['title' => 'My Application'];
+    $response = $this->renderer->render($response, "/index.tpl.php", $data);
+    return $response;
+});
 
 $app->get('/items', function (Request $request, Response $response) {
     $response = $response->getBody()->write('Items list');
