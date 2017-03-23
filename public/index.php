8b0988402d35a12595e314e968a7b1ea54b908da
Myroslav Tkachenko <myroslav.tkachenko@gmail.com>
2017-03-24 01:25:03 +0200

    Install Slim

--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
 <?php
 
-phpinfo();
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+
+$app = new \Slim\App;
+
+$app->get('/', function (Request $request, Response $response) {
+    return $response->getBody()->write('Test');
+});
+
+$app->run();
