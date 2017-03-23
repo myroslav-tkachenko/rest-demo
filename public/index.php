94fe7a30ceb7da89dc688c31f444d880bfc08032
Myroslav Tkachenko <myroslav.tkachenko@gmail.com>
2017-03-24 01:34:10 +0200

    Add basic routes

--- public/index.php
+++ public/index.php
@@ -7,8 +7,29 @@ require '../vendor/autoload.php';
 
 $app = new \Slim\App;
 
-$app->get('/', function (Request $request, Response $response) {
-    return $response->getBody()->write('Test');
+$app->get('/items', function (Request $request, Response $response) {
+    $response = $response->getBody()->write('Items list');
+    return $response;
+});
+
+$app->get('/items/{id}', function (Request $request, Response $response, $args) {
+    $response = $response->getBody()->write('Item ' . $args['id']);
+    return $response;
+});
+
+$app->post('/items', function (Request $request, Response $response) {
+    $response = $response->getBody()->write('Create item');
+    return $response;
+});
+
+$app->put('/items/{id}', function (Request $request, Response $response, $args) {
+    $response = $response->getBody()->write('Update item' . $args['id']);
+    return $response;
+});
+
+$app->delete('/items/{id}', function (Request $request, Response $response, $args) {
+    $response = $response->getBody()->write('Delete item' . $args['id']);
+    return $response;
 });
 
 $app->run();
